9cfb30e761668b1f22f642b2df91c6bbbc197a54
st998814 <116810151+st998814@users.noreply.github.com>
2026-03-25 16:53:13 +1030

    Support Multiple Target Selection

---
+++ AR_gallery/Assets/Scripts/Target/TargetSelectionManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class TargetSelectionManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] targets;
+    [SerializeField] private int activeTargetIndex = 0;
+
+    private void Start()
+    {
+        ShowOnlyActiveTarget();
+    }
+
+    private void Update()
+{
+    if (Keyboard.current.digit1Key.wasPressedThisFrame)
+    {
+        SetActiveTarget(0);
+    }
+
+    if (Keyboard.current.digit2Key.wasPressedThisFrame)
+    {
+        SetActiveTarget(1);
+    }
+}
+    public void SetActiveTarget(int index)
+    {
+        if (index < 0 || index >= targets.Length)
+            return;
+
+        activeTargetIndex = index;
+        ShowOnlyActiveTarget();
+    }
+
+    private void ShowOnlyActiveTarget()
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            if (targets[i] != null)
+            {
+                targets[i].SetActive(i == activeTargetIndex);
+            }
+        }
+    }
+}
